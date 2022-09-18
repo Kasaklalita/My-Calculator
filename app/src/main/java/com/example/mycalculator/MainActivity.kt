5216d5aaee8cd64608358b61404cca8246c8971b
Veniamin Polienko <kasaklalita@mail.ru>
2022-09-18 10:47:41 +0300

    Adding Substraction

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -8,8 +8,8 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var lastNumeric : Boolean = false
-    var lastDot : Boolean = false
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +36,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var tvValue = tvInput.text.toString()
+            var prefix = ""
+            try {
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if (tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                }
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
     fun onOperator(view: View) {
         if (lastNumeric && !isOperatorAdded(tvInput.text.toString())) {
             tvInput.append((view as Button).text)
@@ -44,7 +70,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun isOperatorAdded(value: String) : Boolean {
+    private fun isOperatorAdded(value: String): Boolean {
         return if (value.startsWith("-")) {
             false
         } else {
